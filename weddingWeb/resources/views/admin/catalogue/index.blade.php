3d205a898aea882d072d4ebb7bfc5796f8bf8bbb
Tarmidzi Bariq <81232207+tarmidzibariq@users.noreply.github.com>
2025-09-29 19:03:21 +0700

    Remove jQuery script inclusion from admin catalogue index view

--- weddingWeb/resources/views/admin/catalogue/index.blade.php
+++ weddingWeb/resources/views/admin/catalogue/index.blade.php
@@ -94,7 +94,7 @@
 
 @push('scripts')
 {{-- jquery --}}
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 
 <script>
     $(document).ready(function () {
